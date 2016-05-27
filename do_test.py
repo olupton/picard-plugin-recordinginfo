01d879f25cafd333afaf9f3608d128aff91a6797
Olli Lupton <oliver.lupton@cern.ch>
2016-05-27 21:01:56 +0100

    More examples.

--- do_test.py
+++ do_test.py
@@ -1,2 +1,5 @@
 from extra_relationships import get_recording_live_string
 print get_recording_live_string('ddc9a45a-e3e3-42bb-af05-ea87c17dab63')
+print get_recording_live_string('62c7ac80-13cf-45bf-8ad4-4206a55bd668')
+print get_recording_live_string('3abdd864-aaef-462c-bdc6-6b80eb2642da')
+print get_recording_live_string('8bf6c317-3b14-41e8-a6e8-6948ad8931e2')
